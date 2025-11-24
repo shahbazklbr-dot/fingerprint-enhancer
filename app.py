fec9a81633e610f970ee80ae90ab27d2bfc91901
shahbazklbr-dot <shahbazklbr@gmail.com>
2025-11-24 13:50:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ input[type=file]{
       <button type="submit" class="btn">Enhance</button>
     </div>
   </form>
-  <div class="footer">Powereddd by SecureEnhance — Keep your biometrics private</div>
+  <div class="footer">Poweredddd by SecureEnhance — Keep your biometrics private</div>
 </div>
 <script>
 document.getElementById('uploadForm')?.addEventListener('submit', function(){
